3edcab584d452242b21caf03dacc25a6dc7566c1
Tatiana Titova <ritasom666@gmail.com>
2023-10-29 20:43:48 +0400

    metod

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int tiket = 15_000;
-        int bonus = 20;
-        int mile = tiket / bonus;
-
-        System.out.println("Вам будет начислено " + mile + " миль");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
-
 }
